c3377e1ea58a62c8f73bd4defe9fb1de6fb537a9
Alex <aleckseykalinckovich@gmail.com>
2025-08-06 12:36:18 +0300

    -remove error method

--- src/main/java/com/example/payment_service/repository/PaymentRepository.java
+++ src/main/java/com/example/payment_service/repository/PaymentRepository.java
@@ -3,9 +3,7 @@ package com.example.payment_service.repository;
 import com.example.payment_service.model.Payment;
 import com.example.payment_service.model.PaymentStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface PaymentRepository extends MongoRepository<Payment,String> {
@@ -15,8 +13,4 @@ public interface PaymentRepository extends MongoRepository<Payment,String> {
     List<Payment> getPaymentByUserId(Long userId);
 
     List<Payment> getPaymentByStatus(PaymentStatus status);
-
-    @Query(value = "{}", fields = "{ total: { $sum: \"$paymentAmount\" } }")
-    BigDecimal getTotalAmount();
-
 }
